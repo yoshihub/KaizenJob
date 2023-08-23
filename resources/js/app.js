002e833cea31d1e49d953312238148e7ced05667
yoshihub <saiga123.nm@gmail.com>
2023-08-23 16:04:13 +0900

    Ziggy導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
 createInertiaApp({
 resolve: name => {
@@ -9,6 +10,7 @@ resolve: name => {
 setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
     .use(plugin)
+    .use(ZiggyVue, Ziggy)
     .mount(el)
 },
 })
