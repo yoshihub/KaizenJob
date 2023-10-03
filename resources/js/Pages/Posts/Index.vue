4c29d941d87d7d1ba71fa97f5e0fe3d933495f87
yoshihub <saiga123.nm@gmail.com>
2023-10-04 02:44:32 +0900

    検索フォームの配置調整

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -40,13 +40,13 @@ useFlashToast();
 <App>
     <div>
         <form @submit.prevent="search">
-            <label for="keyword">キーワード</label>
+            <label for="keyword" class="me-1">キーワード</label>
             <input type="text" name="keyword" id="keyword" v-model="searchParams.keyword">
-            <label>日付</label>
+            <label class="ms-3 me-1">日付</label>
             <input type="date" name="date_from" v-model="searchParams.date_from">
             <span>〜</span>
             <input type="date" name="date_to" v-model="searchParams.date_to">
-            <button class="btn btn-primary">検索</button>
+            <button class="btn btn-primary ms-3">検索</button>
         </form>
     </div>
     <div class="row g-3">
